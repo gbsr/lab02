85ff6f278b90736b740a70419ea7caf77240b0c3
gbsr <anders.jakobsen.hofsten@gmail.com>
2023-11-08 09:07:44 +0100

    Initial

---
+++ lab02.js
@@ -0,0 +1,10 @@
+// how many books are there?
+	// loop through list, add to sum
+	// print sum
+
+
+// print all the names of book
+// print all the names of type Fantasy
+// print all names+genre ("classics, dystopia, mystery"))
+// print name and price for all +10$
+
