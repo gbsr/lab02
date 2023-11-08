2d8fbd0f7067275a43c80b588e28f819fc0ab60b
Anton Abrahamsson <abrahamsson37@gmail.com>
2023-11-08 10:02:05 +0100

    Added question 3

--- lab02.js
+++ lab02.js
@@ -1,16 +1,23 @@
-import { books } from './books.js';
-
+import { books } from "./books.js";
 
 listAllBooks(books);
-
+listAllFantasyBooks(books);
 
 // functions
 function listAllBooks(list) {
+    console.log("We currently have these titles:");
+    console.log("");
+    for (var i = 0; i < list.length; i++) {
+        console.log("Title: " + list[i].title);
+    }
+}
 
-	console.log('We currently have these titles:');
-	console.log('');
-	for (var i = 0; i < list.length; i++) {
-		console.log('Title: ' + list[i].title);
-	}
-
+// 3 Skriv ut namn och pris för alla böcker av typen "Fantasy".
+function listAllFantasyBooks() {
+    console.log("These are the books with the Fantasy genre:");
+    books.forEach((book) => {
+        if (book.genre === "Fantasy") {
+            console.log(`${book.title} and it costs ${book.price}`);
+        }
+    });
 }
