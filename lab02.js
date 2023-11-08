b110e15f59c0dd9458393949431648ed2d811e8e
gbsr <anders.jakobsen.hofsten@gmail.com>
2023-11-08 10:01:27 +0100

    added q1

--- lab02.js
+++ lab02.js
@@ -1,16 +1,30 @@
 import { books } from './books.js';
 
-
+listAllItemsInStore(books);
 listAllBooks(books);
 
+// 1 Hur många böcker finns det i affären?
+function listAllItemsInStore(list) {
 
-// functions
-function listAllBooks(list) {
-
-	console.log('We currently have these titles:');
-	console.log('');
-	for (var i = 0; i < list.length; i++) {
-		console.log('Title: ' + list[i].title);
+	let i = 0;
+	let numOfStockItems = 0;
+	for (i = 0; i < books.length; i++) {
+		numOfStockItems++;
 	}
-
+	console.log('WeCurrently have ' + numOfStockItems + ' books in store');
+	console.log('');
 }
+
+// 2 Skriv ut namnen på alla böcker. (Skapa först en lista som bara innehåller namnen.)
+function listAllBooks(list) {
+	let i = 0;
+
+	console.log('These are the titles in store:');
+	console.log('');
+	for (i = 0; i < list.length; i++) {
+		console.log('Title: ' + i + ' ' + list[i].title);
+	}
+}
+
+
+
