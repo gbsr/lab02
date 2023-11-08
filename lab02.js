c695db208f289b1be14c8764618389b0458726ec
gbsr <anders.jakobsen.hofsten@gmail.com>
2023-11-08 09:33:12 +0100

    slight tweak to listAllBooks for readability

--- lab02.js
+++ lab02.js
@@ -7,6 +7,8 @@ listAllBooks(books);
 // functions
 function listAllBooks(list) {
 
+	console.log('We currently have these titles:');
+	console.log('');
 	for (var i = 0; i < list.length; i++) {
 		console.log('Title: ' + list[i].title);
 	}
