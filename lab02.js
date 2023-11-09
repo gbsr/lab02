c877c148f6368b00dbaffd0573be5191b128f2a0
Anton Abrahamsson <abrahamsson37@gmail.com>
2023-11-09 13:36:33 +0100

    Added answer to question 8

--- lab02.js
+++ lab02.js
@@ -6,114 +6,134 @@ import { books } from "./books.js";
 // listBooksByGenre(books, "Classic");								// 4
 // listExpensiveBooks(books);										// 5
 // calculateTotalWorth(books); 									// 6
-// calculateMaxGenreWorth(books, 'Dystopian', 'Classic'); 			// 7 
-// 8
+// calculateMaxGenreWorth(books, 'Dystopian', 'Classic'); 			// 7
+// 8 sortAllTitlesAsc();
 // 9
 // 10
 // 11
 // 12
 // 13
-addBook(books, 'My cup of JavaScript', 'Anders Hofsten', 'Truth', 9.99);// 14
+// addBook(books, 'My cup of JavaScript', 'Anders Hofsten', 'Truth', 9.99);// 14
 
 // 1 Hur många böcker finns det i affären?
 function listAllItemsInStore(books) {
-	let i = 0;
-	let numOfStockItems = 0;
-	for (i = 0; i < books.length; i++) {
-		numOfStockItems++;
-	}
-	console.log("We currently have " + numOfStockItems + " books in store");
-	console.log("");
+    let i = 0;
+    let numOfStockItems = 0;
+    for (i = 0; i < books.length; i++) {
+        numOfStockItems++;
+    }
+    console.log("We currently have " + numOfStockItems + " books in store");
+    console.log("");
 }
 
 // 2 Skriv ut namnen på alla böcker. (Skapa först en lista som bara innehåller namnen.)
 function listAllBooks(books) {
-	let i = 0;
-	console.log("These are the titles in store:");
-	console.log("");
-	for (i = 0; i < books.length; i++) {
-		console.log("Title: " + i + " " + books[i].title);
-	}
+    let i = 0;
+    console.log("These are the titles in store:");
+    console.log("");
+    for (i = 0; i < books.length; i++) {
+        console.log("Title: " + i + " " + books[i].title);
+    }
 }
 
 // 3 Skriv ut namn och pris för alla böcker av typen "Fantasy".
 function listAllFantasyBooks() {
-	console.log("These are the books with the Fantasy genre:");
-	books.forEach((book) => {
-		if (book.genre === "Fantasy") {
-			console.log(`${book.title} and it costs ${book.price}`);
-		}
-	});
+    console.log("These are the books with the Fantasy genre:");
+    books.forEach((book) => {
+        if (book.genre === "Fantasy") {
+            console.log(`${book.title} and it costs ${book.price}`);
+        }
+    });
 }
 // 4 Skriv ut namn och genre för alla klassiker, dystopier och mysterieböcker.
 function listBooksByGenre(books, genre) {
-	console.log(`Böcker inom genren ${genre}:`);
-	books.forEach(book => {
-		if (book.genre === genre) {
-			console.log(`Titel: ${book.title}, Genre: ${book.genre}`);
-		}
-	});
+    console.log(`Böcker inom genren ${genre}:`);
+    books.forEach((book) => {
+        if (book.genre === genre) {
+            console.log(`Titel: ${book.title}, Genre: ${book.genre}`);
+        }
+    });
 }
 
 // 5 Skriv ut namn och pris för alla böcker som kostar över $10.
 function listExpensiveBooks(books) {
-	console.log("Böcker som kostar över $10:");
-	books.forEach(book => {
-		if (book.price > 10) {
-			console.log(`Titel: ${book.title}, Pris: $${book.price}`);
-		}
-	});
+    console.log("Böcker som kostar över $10:");
+    books.forEach((book) => {
+        if (book.price > 10) {
+            console.log(`Titel: ${book.title}, Pris: $${book.price}`);
+        }
+    });
 }
 
 // 6 Hur mycket är hela bokinnehavet värt? (Vad är det totala priset, om man skulle sälja alla böcker?)
 function calculateTotalWorth(books) {
-	let total = 0;
-	let totalSum = 0;
-	books.forEach(book => { total += book.price; });
-	totalSum = Math.round(total);
-	console.log('');
-	console.log('The entire stock of books is currently worth ' + totalSum + ' amount of money-units.');
+    let total = 0;
+    let totalSum = 0;
+    books.forEach((book) => {
+        total += book.price;
+    });
+    totalSum = Math.round(total);
+    console.log("");
+    console.log(
+        "The entire stock of books is currently worth " +
+            totalSum +
+            " amount of money-units."
+    );
 }
 
 // 7 Vilka böcker är sammanlagt värda mest, dystopian eller mystery?
 function calculateMaxGenreWorth(list, genre1, genre2) {
-	// TODO: error checking
+    // TODO: error checking
 
-	let genreA = list.filter(book => book.genre === genre1);
-	let genreB = list.filter(book => book.genre === genre2);
-	let maxGenreWorth = '';
+    let genreA = list.filter((book) => book.genre === genre1);
+    let genreB = list.filter((book) => book.genre === genre2);
+    let maxGenreWorth = "";
 
-	// total here is a var we define as 0
-	let totalPrice_GenreA = genreA.reduce((total, book) => total + book.price, 0);
-	let totalPrice_GenreB = genreB.reduce((total, book) => total + book.price, 0);
+    // total here is a var we define as 0
+    let totalPrice_GenreA = genreA.reduce(
+        (total, book) => total + book.price,
+        0
+    );
+    let totalPrice_GenreB = genreB.reduce(
+        (total, book) => total + book.price,
+        0
+    );
 
-	if (totalPrice_GenreB < totalPrice_GenreA) maxGenreWorth = genre1;
-	else if (totalPrice_GenreB > totalPrice_GenreA) maxGenreWorth = genre2;
+    if (totalPrice_GenreB < totalPrice_GenreA) maxGenreWorth = genre1;
+    else if (totalPrice_GenreB > totalPrice_GenreA) maxGenreWorth = genre2;
 
-	console.log('');
-	if (totalPrice_GenreB === totalPrice_GenreA) console.log('Well, what do you know, the two happens to have the exact same worth :o');
-	else console.log('Comparing the genres ' + genre1 + ' and ' + genre2 + ' , we can see that the genre ' + maxGenreWorth + ' is worth the most.');
-	console.log('');
+    console.log("");
+    if (totalPrice_GenreB === totalPrice_GenreA)
+        console.log(
+            "Well, what do you know, the two happens to have the exact same worth :o"
+        );
+    else
+        console.log(
+            "Comparing the genres " +
+                genre1 +
+                " and " +
+                genre2 +
+                " , we can see that the genre " +
+                maxGenreWorth +
+                " is worth the most."
+        );
+    console.log("");
 }
 
-
-
-
-
-//10 Vilka författare har ett namn som består av mer än 2 ord? 
+//10 Vilka författare har ett namn som består av mer än 2 ord?
 //Ta inte med författare som har punkter i sina namn.
 
 function findAuthorsWithLongNames(books) {
-	const authors = [];
-	books.forEach(book => {
-		const authorName = book.author;
-		const words = authorName.split(' ');
-		const formattedWords = words.filter(word => word !== "");
-		if (formattedWords.length > 2 && !authorName.includes('.')) {
-			authors.push(authorName);
-		}
-	});
-	return authors;
+    const authors = [];
+    books.forEach((book) => {
+        const authorName = book.author;
+        const words = authorName.split(" ");
+        const formattedWords = words.filter((word) => word !== "");
+        if (formattedWords.length > 2 && !authorName.includes(".")) {
+            authors.push(authorName);
+        }
+    });
+    return authors;
 }
 // This needs to be inside the function so that
 
@@ -123,23 +143,51 @@ function findAuthorsWithLongNames(books) {
 
 //11 Skriv ut namnen på alla författare i bokstavsordning. Sortera efter författarens efternamn.
 
-
 // 14 Skriv färdigt funktionen, som ska kunna lägga till en ny bok sist i listan.
 function addBook(list, title, author, genre, price) {
+    const newBook = {
+        title: title,
+        author: author,
+        genre: genre,
+        price: price,
+    };
 
+    console.log("");
+    console.log("Currently our inventory is " + list.length + " books.");
+    console.log("");
 
-	const newBook = { title: title, author: author, genre: genre, price: price };
+    list.push(newBook);
 
-	console.log('');
-	console.log('Currently our inventory is ' + list.length + ' books.');
-	console.log('');
-
-	list.push(newBook);
-
-	console.log('We added ' + list[list.length - 1].title + ' by ' + newBook.author + ' to our inventory; it is filed under the ' + newBook.genre + '-genre.');
-	console.log('The price is set at ' + newBook.price + ' money-units.');
-	console.log('Our inventory is now ' + list.length + ' books and counting!');
+    console.log(
+        "We added " +
+            list[list.length - 1].title +
+            " by " +
+            newBook.author +
+            " to our inventory; it is filed under the " +
+            newBook.genre +
+            "-genre."
+    );
+    console.log("The price is set at " + newBook.price + " money-units.");
+    console.log("Our inventory is now " + list.length + " books and counting!");
 }
 
+// 8 Skriv ut namnen på alla böcker i bokstavsordning.
+function sortAllTitlesAsc() {
+    function sortAllTitles(a, b) {
+        if (a.title < b.title) {
+            return -1;
+        } else if (a.title > b.title) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    const sortedBooks = books.slice().sort(sortAllTitles);
+    console.log(
+        "Here are the titles in alphabetical order: ",
+        sortedBooks.map((book) => book.title)
+    );
+}
 
-
+/* 9 Vilken bok finns det en dubblett av?
+Tips: ni kan skapa en ny lista och använda metoden includes. */
